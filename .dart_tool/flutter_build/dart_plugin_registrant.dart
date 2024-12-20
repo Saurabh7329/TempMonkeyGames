1f46a7d5b76fc58ea6662ddbe0b52ab255361835
Sumit Trivedi <sumittrivedi073@gmail.com>
2024-12-20 15:28:40 +0530

    changes in android section

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -6,24 +6,28 @@
 // @dart = 2.17
 
 import 'dart:io'; // flutter_ignore: dart_io_import.
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:path_provider_android/path_provider_android.dart';
 import 'package:shared_preferences_android/shared_preferences_android.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_android/sqflite_android.dart';
 import 'package:url_launcher_android/url_launcher_android.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
 import 'package:shared_preferences_foundation/shared_preferences_foundation.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_darwin/sqflite_darwin.dart';
 import 'package:url_launcher_ios/url_launcher_ios.dart';
-import 'package:device_info_plus_linux/device_info_plus_linux.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter_local_notifications_linux/flutter_local_notifications_linux.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider_linux/path_provider_linux.dart';
 import 'package:shared_preferences_linux/shared_preferences_linux.dart';
 import 'package:url_launcher_linux/url_launcher_linux.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
 import 'package:shared_preferences_foundation/shared_preferences_foundation.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_darwin/sqflite_darwin.dart';
 import 'package:url_launcher_macos/url_launcher_macos.dart';
-import 'package:device_info_plus_windows/device_info_plus_windows.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider_windows/path_provider_windows.dart';
 import 'package:shared_preferences_windows/shared_preferences_windows.dart';
@@ -35,6 +39,15 @@ class _PluginRegistrant {
   @pragma('vm:entry-point')
   static void register() {
     if (Platform.isAndroid) {
+      try {
+        AndroidFlutterLocalNotificationsPlugin.registerWith();
+      } catch (err) {
+        print(
+          '`flutter_local_notifications` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         PathProviderAndroid.registerWith();
       } catch (err) {
@@ -54,10 +67,10 @@ class _PluginRegistrant {
       }
 
       try {
-        SqflitePlugin.registerWith();
+        SqfliteAndroid.registerWith();
       } catch (err) {
         print(
-          '`sqflite` threw an error: $err. '
+          '`sqflite_android` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -72,6 +85,15 @@ class _PluginRegistrant {
       }
 
     } else if (Platform.isIOS) {
+      try {
+        IOSFlutterLocalNotificationsPlugin.registerWith();
+      } catch (err) {
+        print(
+          '`flutter_local_notifications` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         PathProviderFoundation.registerWith();
       } catch (err) {
@@ -91,10 +113,10 @@ class _PluginRegistrant {
       }
 
       try {
-        SqflitePlugin.registerWith();
+        SqfliteDarwin.registerWith();
       } catch (err) {
         print(
-          '`sqflite` threw an error: $err. '
+          '`sqflite_darwin` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -110,10 +132,19 @@ class _PluginRegistrant {
 
     } else if (Platform.isLinux) {
       try {
-        DeviceInfoLinux.registerWith();
+        DeviceInfoPlusLinuxPlugin.registerWith();
       } catch (err) {
         print(
-          '`device_info_plus_linux` threw an error: $err. '
+          '`device_info_plus` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        LinuxFlutterLocalNotificationsPlugin.registerWith();
+      } catch (err) {
+        print(
+          '`flutter_local_notifications_linux` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -156,6 +187,15 @@ class _PluginRegistrant {
 
     } else if (Platform.isMacOS) {
       try {
+        MacOSFlutterLocalNotificationsPlugin.registerWith();
+      } catch (err) {
+        print(
+          '`flutter_local_notifications` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
         PathProviderFoundation.registerWith();
       } catch (err) {
         print(
@@ -174,10 +214,10 @@ class _PluginRegistrant {
       }
 
       try {
-        SqflitePlugin.registerWith();
+        SqfliteDarwin.registerWith();
       } catch (err) {
         print(
-          '`sqflite` threw an error: $err. '
+          '`sqflite_darwin` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -193,10 +233,10 @@ class _PluginRegistrant {
 
     } else if (Platform.isWindows) {
       try {
-        DeviceInfoWindows.registerWith();
+        DeviceInfoPlusWindowsPlugin.registerWith();
       } catch (err) {
         print(
-          '`device_info_plus_windows` threw an error: $err. '
+          '`device_info_plus` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
